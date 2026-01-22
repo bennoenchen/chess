fbc9748209d3389308edee31dc797f6c2585d50d
Benno Biethan <81190225+bennoenchen@users.noreply.github.com>
2026-01-22 13:45:55 +0900

    some stuff

--- client/game.py
+++ client/game.py
@@ -1,8 +1,20 @@
 import pygame
+from protocol import ProtocolHandler
 
 class Game:
   def __init__(self):
+    protocol = ProtocolHandler()
+
+    gamewidth, gameheight = 800
+    gamewindow(gamewidth, gameheight)
     return
+
+  def test(self):
+    print("Test successful (game.py)")
+
+  def gamewindow(width, height):
+    pygame.display(widht, height)
+    
 
 if __name__ == "__main__"
   newgame = Game()
